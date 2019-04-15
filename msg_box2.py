4000237aee804c11225cff93ce487ba2ad6b1c0b
nwkeith <structor125@gmail.com>
2019-04-15 21:47:37 +0000

    Bread is baked on breadboards

--- msg_box2.py
+++ msg_box2.py
@@ -7,7 +7,27 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+from tkinter import *
+import tkinter.font
+from gpiozero import LED
+import RPi.GPIO
 
+RPi.GPIO.setmode(RPi.GPIO.BCM)
+led = LED(23)
+
+def ledToggle():
+    if led.is_lit:
+        led.off()
+        #ledButton["text"] = "Turn LED on"
+    else:
+        led.on()
+        #ledButton["text"] = "Turn LED off"
+
+def btn_clicked():
+    #print("Button Pressed")
+    #QMessageBox.information(MainWindow, 'Welcome', 'PyQt5 + Raspberry PI')
+    ledToggle()
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
@@ -20,10 +40,6 @@ class Ui_MainWindow(object):
         font.setPointSize(28)
         self.pushButton.setFont(font)
         self.pushButton.setObjectName("pushButton")
-        self.verticalSlider = QtWidgets.QSlider(self.centralwidget)
-        self.verticalSlider.setGeometry(QtCore.QRect(120, 200, 22, 160))
-        self.verticalSlider.setOrientation(QtCore.Qt.Vertical)
-        self.verticalSlider.setObjectName("verticalSlider")
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
@@ -36,4 +52,13 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Message Box"))
         self.pushButton.setText(_translate("MainWindow", "Click Me!"))
+        self.pushButton.clicked.connect(btn_clicked)
+
+import sys
+app=QtWidgets.QApplication(sys.argv)
+MainWindow=QtWidgets.QMainWindow()
+ui=Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.show()
+sys.exit(app.exec_())
 
